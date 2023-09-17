a93177732056a1a9d5b951c176d8b51e827c1671
hibaqw <hibaqwarsame79@gmail.com>
2023-09-17 12:40:13 -0600

    edited login form to extract email data

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,20 +7,23 @@
       <div class="py-14" />
       Enter the email address associated with your account, and we’ll send a magic link to your inbox.
       <div class="py-14" />
-      <v-row justify="center" class="ma-2">
+      <!-- <v-row justify="center" class="ma-2">
         <v-col cols="4"><v-text-field label="Email" placeholder="Email"></v-text-field></v-col>
       </v-row>
       <v-btn
           color="primary"
         >
         Continue
-        </v-btn>
-
+        </v-btn> -->
+      <form>
+        <p>email: <input type="text" required></p>
+        <button type="submit">continue</button>
+      </form>
     </v-responsive>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-//
+
 
 </script>
