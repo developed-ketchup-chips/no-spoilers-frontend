d40ea79ae955e47bd9a3d1d0e25ce7e104ee58f8
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-09-16 18:07:27 -0600

    added dialog to home, added colours to page, formating the dialog

--- src/components/RoomAdd.vue
+++ src/components/RoomAdd.vue
@@ -15,39 +15,22 @@
       </template>
       <v-card>
         <v-card-title>
-          <span class="text-h5">User Profile</span>
+          <span class="text-h5">Create A Room</span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
+
+
+
               <v-col
                 cols="12"
                 sm="6"
                 md="4"
               >
                 <v-text-field
-                  label="Legal first name*"
-                  required
-                ></v-text-field>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-text-field
-                  label="Legal middle name"
-                  hint="example of helper text only on focus"
-                ></v-text-field>
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
+                  label="Name of TV show*"
+                  hint="Dont forget to fill me in"
                   persistent-hint
                   required
                 ></v-text-field>
@@ -57,7 +40,7 @@
                 cols="12"
                 sm="6"
               >
- <v-autocomplete
+              <v-autocomplete
                   :items="['Book', 'TV Show', 'Movie']"
                   label="Media Type"
                   multiple
@@ -71,12 +54,11 @@
                   label="Media Length*"
                   required
                 ></v-text-field>
-
-
-
               </v-col>
             </v-row>
           </v-container>
+
+
           <small>*indicates required field</small>
         </v-card-text>
         <v-card-actions>
